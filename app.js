b37d8408b92403a7891e3e3d5c149776de733235
Jeff <jho1211@students.cs.ubc.ca>
2025-03-29 20:57:08 -0700

    fix api routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 
 const uploadRoutes = require("./routes/upload");
 const transcriptRoutes = require('./routes/transcripts'); // post route at path
+const User = require("./models/userSchema");
 
 require("dotenv").config();
 
@@ -32,89 +33,6 @@ app.listen(port, () => {
 app.use("/upload", uploadRoutes);
 
 app.use("/", express.static("public"));
-
-
-// Multer setup for storing audio files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) =>
-    cb(
-      null,
-      new Date(Date.now())
-        .toLocaleString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: false, // Use 24-hour format
-        })
-        .replace(/:/g, "-") + path.extname(file.originalname)
-    ),
-});
-
-const upload = multer({ storage });
-
-app.post("/upload", upload.single("audio"), async (req, res) => {
-  console.log("called upload");
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
-  try {
-    const audioPath = req.file.path;
-    console.log(audioPath);
-
-    // Send the file to the Flask server for transcription
-    const formData = new FormData();
-    const fileStream = fs.createReadStream(audioPath);
-    // console.log(fileStream);
-    formData.append("audio", fileStream);
-
-    console.log("calling flask server");
-    // console.log(formData.getHeaders());
-    const response = await fetch("http://127.0.0.1:3001/transcribe", {
-      method: "POST",
-      body: formData,
-      headers: formData.getHeaders(),
-    });
-
-    const transcript = await response.json();
-    console.log(transcript.transcript);
-
-    // quick test upload todb
-    const Transcript = require("./public/models/transcript.js");
-    const newTranscript = new Transcript({
-      callSign: "Alpha123",
-      fileNumber: "12345",
-      dispatcherId: "Disp001",
-      transcript: transcript.transcript,
-      location: { lat: 34.0522, lng: -118.2437 }, // Example coordinates
-      callId: "Call001",
-      isEmergency: true,
-      transcriptionStatus: "processing",
-    });
-    newTranscript
-      .save()
-      .then((savedTranscript) => {
-        console.log("Transcript saved:", savedTranscript);
-      })
-      .catch((error) => {
-        console.error("Error saving transcript:", error);
-      });
-
-    // Cleanup
-    fs.unlinkSync(audioPath);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.toString() });
-  }
-});
-
-// Ensure uploads directory exists
-const uploadDir = path.join(__dirname, "uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-const User = require("./models/userSchema");
 
 app.post("/check-callsign", async (req, res) => {
   const { callSign } = req.body;
